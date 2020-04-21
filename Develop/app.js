91dfdb07d3736135c29e2e8e9cc5d8c045ac2de3
Massimo Guida <massimo.guidamusic@gmail.com>
2020-04-21 13:33:32 -0400

    update assignment 10 so that an HTML file is written

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,4 @@
+// Variables
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -13,9 +14,9 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 const axios = require("axios");
-
 const employeesArray = [];
 
+// Create server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -29,7 +30,11 @@ server.listen(port, hostname, () => {
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+  // HINT: each employee type (manager, engineer, or intern) has slightly different
+  // information; write your code to ask different questions via inquirer depending on
+  // employee type.
 
+// Run prompts for manager
 inquirer.prompt([
     {
     type: "confirm",
@@ -58,6 +63,12 @@ inquirer.prompt([
       name: "officeNumber"
     },
     {
+      type: "confirm",
+      message: "Would you like to add a new employee to your team?",
+      name: "confirmEmployee",
+      default: true
+      },
+    {
       type: "list",
       message: "What is your employee's role in the company?",
       choices: ['Engineer', 'Intern'],
@@ -74,8 +85,9 @@ inquirer.prompt([
     )
 
     employeesArray.push(managerInfo);
-    console.log(employeesArray);
+    // console.log(employeesArray);
 
+    // Run prompts for Engineer
     if (response.role === 'Engineer') {
 
       inquirer.prompt([
@@ -106,20 +118,27 @@ inquirer.prompt([
           getGitHubProfileInfo(answers.github);
 
           const engineerInfo = new Engineer(
-          answers.name, 
-          answers.id,
-          answers.email, 
-          answers.github
+            answers.name, 
+            answers.id,
+            answers.email, 
+            answers.github
         )
           employeesArray.push(engineerInfo);
           console.log(employeesArray);
-          render(employeesArray);
+          let renderHTML = render(employeesArray);
+          console.log(renderHTML);
+
+          fs.writeFile('./output/team.html', renderHTML, function (err) {
+            if (err) throw err;
+            console.log('Team added!');
+          });
 
         });
 
     }
     else {
 
+      // Run prompts for Intern
       inquirer.prompt([
         {
           type: "input",
@@ -151,51 +170,50 @@ inquirer.prompt([
            answers.school
         )
         
+        // After the user has input all employees desired, call the `render` function (required
+        // above) and pass in an array containing all employee objects; the `render` function will
+        // generate and return a block of HTML including templated divs for each employee!
         employeesArray.push(internInfo);
-        console.log(employeesArray);
-        render(employeesArray);
+        // console.log(employeesArray);
+        let renderHTML = render(employeesArray);
+        // console.log(renderHTML);
+
+        // After you have your html, you're now ready to create an HTML file using the HTML
+        // returned from the `render` function. Now write it to a file named `team.html` in the
+        // `output` folder. You can use the variable `outputPath` above target this location.
+          
+          // Hint: you may need to check if the `output` folder exists and create it if it
+          // does not.
+          
+        fs.writeFile('./output/team.html', renderHTML, function (err) {
+          if (err) throw err;
+          console.log('Team added!');
+        });
 
         });
     } 
+
+    // Close server
     server.close();
 
   })
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-
-    // Hint: you may need to check if the `output` folder exists and create it if it
-    // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-
+// Call the GitHub api to retrieve information from the Engineer's GitHub profile
 async function getGitHubProfileInfo(user) {
 
   const { data } = await axios.get(
     `https://api.github.com/users/${user}`
   );
 
-  // console.log(data);
-
   const stringData = JSON.stringify(data, null, '  ');
-
-  // console.log(stringData);
 
 }
 
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an 
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work!```
 
 // // Rough
 // const addEmployee = [
@@ -302,57 +320,3 @@ async function getGitHubProfileInfo(user) {
 // server.close();
 
 // }
-
-//   const result = `
-//   <!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//       <meta charset="UTF-8">
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-//       <title>My Team</title>
-//   </head>
-//   <body>
-//       <div class="container">
-//           <div class="row">
-//               <div class="col-sm">
-//                   <div class="jumbotron jumbotron-fluid text-white bg-danger text-center">
-//                       <div class="container">
-//                       <h1 class="display-4">My Team</h1>
-//                       </div>
-//                   </div>
-//               </div>
-//           </div>
-//           <div class="row">
-//               <div class="col-sm">
-//                   <div class="card border-secondary" style="width: 18rem;">
-//                       <div class="card-body text-white bg-primary">
-//                         <h4 class="card-title">${answers.name}</a></h4>
-//                         <h5 class="card-text">${answers.employee}</h5>
-//                       </div>
-//                       <div class="container bg-light">
-//                           <div class="card-body">
-//                               <ul class="list-group list-group-flush">
-//                                 <li class="list-group-item">ID:${answers.id}</li>
-//                                 <li class="list-group-item"><a href="mailto:${answers.email}" target="_blank">${answers.email}</a></li>
-//                                 <li class="list-group-item">Employee info:${response.school}</li>
-//                               </ul>
-//                           </div>
-//                       </div>
-//                   </div>
-//               </div>
-//           </div>
-//       </div>
-//   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-//   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-//   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-//   </body>
-//   </html>
-
-//   `;
-
-// // e-mail link to populate card
-// <a href="mailto:${answers.email}" target="_blank">${answers.email}</a>
-
-// // GitHub link to populate card
-// <a href="${stringData}" target="_blank">${answers.github}</a>
--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 The following application allows a manager to generate a team html page through the CLI by prompting them for a series of questions on professional information about themselves and their employees through node.
 
+## Bugs
+
+* Although the team page is successfully created from the prompts, at the moment, the page only contains the manager and one employee, as I am unsure how to loop the prompts so that more employees can be added to the team
+
+* Moreover, I am unsure how to allow the user to continue with the prompts if the answer of the confirm is "yes," or to end the session if the answer of the confirms is "no"
 
 ## Deployed links:
 
