36b793f37476b3d5a647338e2711c7d2360531fc
Massimo Guida <massimo@massimoguida.com>
2020-05-16 02:13:21 -0400

    fixed server file CLI so that all team members can be added & adjusted readme file

--- Develop/app.js
+++ Develop/app.js
@@ -10,8 +10,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const util = require("util");
-const http = require('http');
-const hostname = '127.0.0.1';
+const http = require("http");
+const hostname = "127.0.0.1";
 const port = 3000;
 const axios = require("axios");
 const employeesArray = [];
@@ -19,304 +19,261 @@ const employeesArray = [];
 // Create server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
-  // console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
+  runCLI();
 });
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-  // HINT: each employee type (manager, engineer, or intern) has slightly different
-  // information; write your code to ask different questions via inquirer depending on
-  // employee type.
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
 
-// Run prompts for manager
-inquirer.prompt([
-    {
-    type: "confirm",
-    message: "Are you the team manager?",
-    name: "confirmManager",
-    default: true
-    },
-    {
-      type: "input",
-      message: "What is your full name?",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "What is your user email?",
-      name: "email"
-    },
-    {
-      type: "input",
-      message: "What is your id?",
-      name: "id"
-    },
-    {
-      type: "input",
-      message: "What is your office number?",
-      name: "officeNumber"
-    },
-    {
+// Run command line interface
+function runCLI() {
+  inquirer
+    // Prompt the user asking them what they want to do
+    .prompt({
       type: "confirm",
-      message: "Would you like to add a new employee to your team?",
-      name: "confirmEmployee",
+      message: "Are you the team manager?",
+      name: "confirmManager",
       default: true
-      },
-    {
-      type: "list",
-      message: "What is your employee's role in the company?",
-      choices: ['Engineer', 'Intern'],
-      name: "role"
-    }
-  ])
-  .then(function(response) {
+    })
+    // Depending on their answer, initiate a particular function
+    .then(function (answer) {
+      if (answer.confirmManager === true) {
+        addManager();
+      } 
+      else {
+        // Close server
+        server.close();
+      }
+    });
+}
 
-    const managerInfo = new Manager(
-       response.name, 
-       response.id,
-       response.email, 
-       response.officeNumber,
-    )
-
-    employeesArray.push(managerInfo);
-    // console.log(employeesArray);
-
-    // Run prompts for Engineer
-    if (response.role === 'Engineer') {
-
-      inquirer.prompt([
-        {
-          type: "input",
-          message: "What is your Engineer's full name?",
-          name: "name"
-        },
-        {
-          type: "input",
-          message: "What is your Engineer's email?",
-          name: "email"
-        },
-        {
-          type: "input",
-          message: "What is your Engineer's id?",
-          name: "id"
-        },
-        {
+// Function for add a manager
+function addManager() {
+  // Prompt the manager with some questions about themselves
+  inquirer
+    .prompt([
+      {
         type: "input",
-        message: "What is your Engineer's GitHub username?",
-        name: "github"
-        }
-      ])
-      .then(answers => {
-        // call getGitHubProfileInfo function
-          // console.log(JSON.stringify(answers, null, '  '));
-          getGitHubProfileInfo(answers.github);
+        message: "What is your full name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your user email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your id?",
+        name: "id"
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "officeNumber"
+      },
+      {
+        type: "confirm",
+        message: "Would you like to add a new employee to your team?",
+        name: "confirmEmployee",
+        // default: true
+      }
+    ])
+    // Depending on their answer, initiate a particular function
+    .then(function (answer) {
+      const managerInfo = new Manager(
+        answer.name,
+        answer.id,
+        answer.email,
+        answer.officeNumber
+      );
 
-          const engineerInfo = new Engineer(
-            answers.name, 
-            answers.id,
-            answers.email, 
-            answers.github
-        )
-          employeesArray.push(engineerInfo);
-          console.log(employeesArray);
-          let renderHTML = render(employeesArray);
-          console.log(renderHTML);
+      employeesArray.push(managerInfo);
 
-          fs.writeFile('./output/team.html', renderHTML, function (err) {
-            if (err) throw err;
-            console.log('Team added!');
-          });
+      if (answer.confirmEmployee === true) {
+        addEmployee();
+      } 
+      else {
 
+        let renderHTML = render(employeesArray);
+  
+        fs.writeFile("./output/team.html", renderHTML, function (err) {
+          if (err) throw err;
+          console.log("Team added!");
         });
 
-    }
-    else {
+        // Close server
+        server.close();
+      }
+    });
+}
 
-      // Run prompts for Intern
-      inquirer.prompt([
-        {
-          type: "input",
-          message: "What is your Intern's name?",
-          name: "name"
-        },
-        {
-          type: "input",
-          message: "What is your Intern's email?",
-          name: "email"
-        },
-        {
-          type: "input",
-          message: "What is Intern's id?",
-          name: "id"
-        },
-        {
+// Function for adding a new employee
+function addEmployee() {
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What is your employee's role in the company?",
+        choices: [
+          "Engineer", 
+          "Intern"
+        ],
+        name: "role",
+      },
+    ])
+    .then(function (answer) {
+      // Depending on their answer, initiate a particular function for adding an engineer or intern
+      if (answer.role === "Engineer") {
+        addEngineer();
+      } else if (answer.role === "Intern") {
+        addIntern();
+      }
+    });
+}
+
+
+// Function for adding an engineer
+function addEngineer() {
+  // Prompt the manager with quesions asking them about information regarding their engineer
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your Engineer's full name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your Engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your Engineer's id?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your Engineer's GitHub username?",
+        name: "github",
+      }
+    ])
+    // Depending on their answer, initiate a particular function
+    .then((answers) => {
+      // call getGitHubProfileInfo function
+      getGitHubProfileInfo(answers.github);
+
+      const engineerInfo = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      employeesArray.push(engineerInfo);
+      addNewEmployee();
+    });
+}
+
+// Function for adding an intern
+function addIntern() {
+  // Prompt the manager with quesions asking them about information regarding their intern
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your Intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is your Intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is Intern's id?",
+        name: "id",
+      },
+      {
         type: "input",
         message: "What university is your Intern currently attending?",
-        name: "school"
-        }
-      ])
-      .then(answers => {
+        name: "school",
+      }
+    ])
+    // Depending on their answer, initiate a particular function
+    .then((answers) => {
+      const internInfo = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
 
-        const internInfo = new Intern(
-           answers.name, 
-           answers.id,
-           answers.email, 
-           answers.school
-        )
-        
-        // After the user has input all employees desired, call the `render` function (required
-        // above) and pass in an array containing all employee objects; the `render` function will
-        // generate and return a block of HTML including templated divs for each employee!
-        employeesArray.push(internInfo);
-        // console.log(employeesArray);
+      // After the user has input all employees desired, call the `render` function (required
+      // above) and pass in an array containing all employee objects; the `render` function will
+      // generate and return a block of HTML including templated divs for each employee!
+      employeesArray.push(internInfo);
+      addNewEmployee();
+    });
+}
+
+// Function for adding a new employee
+function addNewEmployee() {
+
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        message: "Would you like to add a new employee to your team?",
+        name: "confirmNewEmployee",
+        default: true,
+      },
+    ])
+    // Depending on their answer, initiate a particular function
+    .then(function (answer) {
+      if (answer.confirmNewEmployee === true) {
+        addEmployee();
+      } else {
+        // Close server
         let renderHTML = render(employeesArray);
-        // console.log(renderHTML);
-
+  
         // After you have your html, you're now ready to create an HTML file using the HTML
         // returned from the `render` function. Now write it to a file named `team.html` in the
         // `output` folder. You can use the variable `outputPath` above target this location.
-          
-          // Hint: you may need to check if the `output` folder exists and create it if it
-          // does not.
-          
-        fs.writeFile('./output/team.html', renderHTML, function (err) {
+  
+        // Hint: you may need to check if the `output` folder exists and create it if it
+        // does not.
+  
+        fs.writeFile("./output/team.html", renderHTML, function (err) {
           if (err) throw err;
-          console.log('Team added!');
+          console.log("Team added!");
         });
-
-        });
-    } 
-
-    // Close server
-    server.close();
-
-  })
+        server.close();
+      }
+    });
+}
 
 // Call the GitHub api to retrieve information from the Engineer's GitHub profile
 async function getGitHubProfileInfo(user) {
+  const { data } = await axios.get(`https://api.github.com/users/${user}`);
 
-  const { data } = await axios.get(
-    `https://api.github.com/users/${user}`
-  );
-
-  const stringData = JSON.stringify(data, null, '  ');
-
+  const stringData = JSON.stringify(data, null, "  ");
 }
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
+// for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
-
-// // Rough
-// const addEmployee = [
-//   {
-//     type: "confirm",
-//     message: "Do you want to add a new employee to your team?",
-//     name: "addEmployee",
-//     default: true
-//   }
-// ];
-
-// const generalQuestions = [
-//   {
-//     type: "input",
-//     message: "What is your full name?",
-//     name: "name"
-//   },
-//   {
-//     type: "input",
-//     message: "What is your user email?",
-//     name: "email"
-//   },
-//   {
-//     type: "input",
-//     message: "What is your id?",
-//     name: "id"
-//   },
-//   {
-//     type: "list",
-//     message: "What is your role in the company?",
-//     choices: ['Engineer', 'Intern', 'Manager'],
-//     name: "role"
-//   }
-// ];
-
-// const engineerQuestion = [
-// {
-//   type: "input",
-//   message: "What is your GitHub username?",
-//   name: "github"
-//   }
-// ];
-
-// const internQuestion = [
-//   {
-//     type: "input",
-//     message: "What university are you currently attending?",
-//     name: "school"
-//     }
-//   ];
-
-// const managerQuestion = [
-//   {
-//     type: "input",
-//     message: "What is your office number?",
-//     name: "officeNumber"
-//     }
-//   ];
-
-// // let question = "1";
-// // While (question !== "done") {
-// //   If (question == "1") {
-// //     Let answer = await inquirer(addEmployee);
-// //     question = (answer == 'yay') ? "2" : "1";
-// //   } else if (question == "2") {
-// //     Let answer = await inquirer(generalQuestions);
-// //     If (answer == 'barks') { 
-// //       question = "done";
-// //     } else if (answer == 'dog') {
-// //       question = "1";
-// //     } else {
-// //       question = "2";
-// //     }
-// //   }
-// // }
-
-
-
-// inquirer.prompt(questions).then(answers => {
-//   // call getGitHubProfileInfo function
-//     console.log(JSON.stringify(answers, null, '  '));
-//     getGitHubProfileInfo(answers.github, answers.email, answers.repo, answers.projectTitle);
-
-//   });
-
-// inquirer.prompt()
-
-//       if (response.addEmployee) {
-
-//     }
-
-//     else {
-
-//       server.close();
-
-//     }
-
-// async function generateHTML(userName, userPosition, userID, userEmail, userInfo){
-
-//   fs.writeFile("./output/team.html", response, function (err) {
-//     if (err) return console.log(err);    
-//   });
-
-// server.close();
-
-// }
--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 
 The following application allows a manager to generate a team html page through the CLI by prompting them for a series of questions on professional information about themselves and their employees through node.
 
-## Bugs
+## Usage
+In order to run the appication: 
+* Clone the [GitHub repository](https://github.com/MG-cpu90/assignment10) into a new folder on your computer 
+* The repository contains a package.json file, so you will need to run "npm install" in your terminal in order to install the necessary pacakges
+* Open the "app.js" file in your terminal window, and type in "app.js" to run the CLI application
+* You will be prompted for a series of questions and once they have all been answered, a webpage ("team.html") with inforation about your team will be generated in the "output" folder
+
+### User Story
+```
+AS a manager
+I WANT to generate a webpage that displays my team's basic info
+SO THAT I have quick access to emails and GitHub profiles
+```
+
+## Bugs (Now Resolved)
 
 * Although the team page is successfully created from the prompts, at the moment, the page only contains the manager and one employee, as I am unsure how to loop the prompts so that more employees can be added to the team
 
