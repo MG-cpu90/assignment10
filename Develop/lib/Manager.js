58d34b20000b8735fae37a2dc32428dd575fa293
Massimo Guida <massimo.guidamusic@gmail.com>
2020-09-02 10:37:29 -0400

    fixed comment for Manager

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,4 +1,4 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// Code to define and export the Manager class, which inherits from Employee.
 
 const Employee = require("./Employee");
 
@@ -8,7 +8,7 @@ class Manager extends Employee {
         super(name, id, email)
         this.officeNumber = officeNumber;
 
-    }  
+    }
 
     getRole() {
         return "Manager";
